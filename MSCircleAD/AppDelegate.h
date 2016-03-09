2b615da01ee6148bed6730635872ec1b21841176
mr.scorpion <1515679729@qq.com>
2016-03-09 22:08:50 +0800

    Initial Commit

---
+++ MSCircleAD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSCircleAD
+//
+//  Created by mr.scorpion on 16/3/9.
+//  Copyright © 2016年 mr.scorpion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
